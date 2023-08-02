69b93f7f53bc391e02cb99b95149fa37d1d7026a
GeorginaCarlisle <>
2023-08-02 14:14:55 +0000

    print the next days stock values back to the user as a dictionary

--- run.py
+++ run.py
@@ -109,6 +109,17 @@ def calculate_stock_data(data):
     
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Get and print the next days stock values as a dictionary
+    with the sandwich names as the key 
+    and the number of sandwiches to be made as the value
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+    stock_dictionary = {headings[i]: data[i] for i in range(len(headings))}
+    print("Make the following sandwiches for the next event:")
+    print(stock_dictionary)
+
 def main():
     """
     Run all program functions
@@ -121,6 +132,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
